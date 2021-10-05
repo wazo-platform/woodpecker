38d997107c1f811cddb7de463b15d069bdf5f79d
Romej <jclaveau@wazo.io>
2021-10-05 17:19:51 -0400

    redirect logic

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import Wazo from '@wazo/sdk/lib/simple';
 
@@ -15,19 +15,47 @@ export default function App() {
       const newSession = await Wazo.Auth.logIn(username, password);
       await Wazo.Phone.connect();
       setSession(newSession);
+
+      localStorage.setItem('token', newSession.token);
+      localStorage.setItem('refreshToken', newSession.refreshToken);
+      localStorage.setItem('server', server);
     } catch (error) {
-      console.error('Auth error', error);
+      console.error('Auts error', error);
       return;
     }
-  }
+  };
+
+  useEffect(() => {
+    const validateToken = async (token: string) => {
+      const refreshToken = localStorage.getItem('refreshToken');
+      const existingSession = refreshToken
+        ? await Wazo.Auth.validateToken(token)
+        : await Wazo.Auth.validateToken(token, refreshToken);
+
+      return existingSession;
+    };
+
+    Wazo.Auth.init('woodpecker', 10);
+    const server = localStorage.getItem('server');
+    const token = localStorage.getItem('token');
+
+    if (server && token) {
+      Wazo.Auth.setHost(server);
+      const validToken = validateToken(token);
+      if (validToken) {
+        console.log('redirect !')
+        // redirect to a different page
+      }
+    }
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text>Woody !</Text>
-      <TextInput onChangeText={setUsername} placeholder="Username" />
-      <TextInput onChangeText={setPassword} placeholder="Password" secureTextEntry />
-      <TextInput onChangeText={setServer} placeholder="Server" />
-      <Button onPress={login} title="Login" />
+      <TextInput onChangeText={setUsername} placeholder='Username' />
+      <TextInput onChangeText={setPassword} placeholder='Password' secureTextEntry />
+      <TextInput onChangeText={setServer} placeholder='Server' />
+      <Button onPress={login} title='Login' />
     </View>
   );
 }
